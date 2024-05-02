c5f79b7befb489951552dd41b470c5e0913ae925
Leonardo <leosouza5@hotmail.com.br>
2024-05-02 19:45:16 -0400

    adicionando package entidades

---
+++ ProjetoP2/src/main/java/br/unigran/app/projetop2/entidades/Produto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.unigran.app.projetop2.entidades;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Produto {
+    
+}
