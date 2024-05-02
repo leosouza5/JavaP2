0db6dd7c9bdd966eb6131b166a2bf475802d3c3d
Leonardo <leosouza5@hotmail.com.br>
2024-05-02 19:36:22 -0400

    Primeiro Commit

---
+++ ProjetoP2/src/main/java/br/unigran/app/projetop2/ProjetoP2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unigran.app.projetop2;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class ProjetoP2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
