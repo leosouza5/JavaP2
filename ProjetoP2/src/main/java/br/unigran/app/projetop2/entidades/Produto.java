2ea2700821b40f6a07f12540e35f1c67c306158c
Leonardo <leosouza5@hotmail.com.br>
2024-05-02 20:20:32 -0400

    Criando entidade Produto

--- ProjetoP2/src/main/java/br/unigran/app/projetop2/entidades/Produto.java
+++ ProjetoP2/src/main/java/br/unigran/app/projetop2/entidades/Produto.java
@@ -4,10 +4,23 @@
  */
 package br.unigran.app.projetop2.entidades;
 
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
 /**
  *
  * @author laboratorio
  */
+@Getter
+@Setter
 public class Produto {
-    
+    private Integer id;
+    private String marca;
+    private String cateoria;
+    private Double precoCusto;
+    private Double precoVenda;
+    private Fornecedor idFornecedor;
+    private Date dataValidade;
+    private Double qtdMinimo;
+    private Double qtdEstoque;
 }
