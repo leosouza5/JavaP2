6ca396e8689fcd116689a17aaa389512c6b18257
Leonardo <leosouza5@hotmail.com.br>
2024-05-09 20:13:40 -0400

    Concertando meus erros

--- ProjetoP2/src/main/java/br/unigran/app/projetop2/entidades/NotaFiscal.java
+++ ProjetoP2/src/main/java/br/unigran/app/projetop2/entidades/NotaFiscal.java
@@ -1,11 +1,26 @@
 
 package br.unigran.app.projetop2.entidades;
 
-public class NotaFiscal {
+import java.io.Serializable;
+import javax.annotation.processing.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+@Entity
+public class NotaFiscal implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "cnpj")
     private String cnpj;
+    @Column(name = "nome")
     private String nome;
+    
     private Endereco endereco;
     private Entrada entrada;
+    @OneToOne(mappedBy = "notaFiscal")
     private Saida saida;
 }
