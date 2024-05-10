336db5d35f0b37d4d570463ca5d3ba02bb0328ab
Leonardo <leosouza5@hotmail.com.br>
2024-05-09 20:15:05 -0400

    Finalizando algumas entidades e JPA

--- ProjetoP2/src/main/java/br/unigran/app/projetop2/entidades/Cidade.java
+++ ProjetoP2/src/main/java/br/unigran/app/projetop2/entidades/Cidade.java
@@ -1,13 +1,30 @@
 
 package br.unigran.app.projetop2.entidades;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Cidade {
+@Entity
+public class Cidade implements Serializable{
+
+    @OneToMany(mappedBy = "cidade")
+    private List<Endereco> enderecos;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name="nome",length = 55)
     private String nome;
+    @ManyToOne
     private Estado estado;
 }
--- ProjetoP2/src/main/java/br/unigran/app/projetop2/entidades/Estado.java
+++ ProjetoP2/src/main/java/br/unigran/app/projetop2/entidades/Estado.java
@@ -1,13 +1,28 @@
 
 package br.unigran.app.projetop2.entidades;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Estado {
+@Entity
+public class Estado implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @OneToMany(mappedBy = "estado")
+    private List<Cidade> cidades;
+    @Column(name = "nmEstado",length = 55)
     private String nmEstado;
+    @Column(name = "sigla",length = 2)
     private String sigla;
 }
